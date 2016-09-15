0aa6dfa3466e02114fa42929824184a8a1278898
NGUYEN QUOC HUY <nerorain1102@gmail.com>
2016-09-15 17:48:51 +0700

    change js

--- resources/views/frontend/pages/artists.blade.php
+++ resources/views/frontend/pages/artists.blade.php
@@ -68,7 +68,7 @@
             <div class='masonry-brick'>
                 <article class='model grid-item' data-artist='#=base64 #'>
                     <a href='/nghe-si/#=data[i].slug#' >
-                        <div class='model-img-wrapper model-background-img-wrapper' style='background-image: url()'>
+                        <div class='model-img-wrapper model-background-img-wrapper' style='background-image: url(#=data[i].file_id#.#=data[i].extension#)'>
                             <div class='model-name-box'>
                                 <span class='model-name' data-name='#=data[i].full_name#'></span> </div>
                             <div class='wishlist-icon-wrapper in-wishlist' >
